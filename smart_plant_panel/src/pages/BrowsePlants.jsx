ef10c3be4709ca50f9388ade88082795a54712fe
Federico <alessi.frc@gmail.com>
2025-07-03 23:24:31 +0200

    added toasters and improved add to nursery

--- smart_plant_panel/src/pages/BrowsePlants.jsx
+++ smart_plant_panel/src/pages/BrowsePlants.jsx
@@ -3,19 +3,29 @@ import FetchPlants from '../Components/FetchPlants';
 import Plant from '../Components/Plant';
 import { addToNursery } from '../redux/NurseryActions';
 import { useDispatch } from 'react-redux';
+import Alert from '@mui/material/Alert';
 
 const BrowsePlants = () => {
     const [selectedPlant, setSelectedPlant] = useState(null)
     const dispatch = useDispatch()
+    const [flag, setFlag] = useState(false)
+
+    const nurseryHandler = () => {
+        dispatch(addToNursery(selectedPlant.id))
+        setSelectedPlant(null)
+        setFlag(true)
+        setTimeout(() => {setFlag(false)}, 5000)
+    }
     
     return (
         <div>
+            {flag ? <Alert severity='success' onClose={()=>{setFlag(false)}} id='alert'>Plant added to nursery</Alert> : null}
             <br/>
             {selectedPlant ? (
                 <div>
                 <button onClick={() => setSelectedPlant(null)}>↩</button>
                 <Plant {...selectedPlant} />
-                <button onClick={() => dispatch(addToNursery(selectedPlant.id))}>Add to nursery</button>
+                <button onClick={() => nurseryHandler()}>Add to nursery</button>
                 </div>
             ) : (
                 <FetchPlants onSelectPlant={setSelectedPlant}/>)}
--- smart_plant_panel/src/pages/MyNursery.jsx
+++ smart_plant_panel/src/pages/MyNursery.jsx
@@ -4,9 +4,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import Plant from '../Components/Plant';
 import { useNavigate } from 'react-router';
 import { removeFromNursery } from '../redux/NurseryActions';
+import Alert from '@mui/material/Alert';
 
 const MyNursery = () => {
     const [selectedPlant, setSelectedPlant] = useState(null)
+    const [flag, setFlag] = useState(false) //flag for the alert
     const myPlants = useSelector((state) => state.nursery.plants)
     const navigate = useNavigate()
     const dispatch = useDispatch()
@@ -14,11 +16,14 @@ const MyNursery = () => {
     const removeHandler = () => {
         dispatch(removeFromNursery(selectedPlant.id))
         setSelectedPlant(null)
+        setFlag(true)
+        setTimeout(() => {setFlag(false)}, 5000)
     }
-    
+
     console.log(myPlants)
     return (
         <div>
+            {flag && <Alert severity='info' id='alert' onClose={() => {setFlag(false)}}>Plant removed from nursery</Alert>}
             <h1>Nursery</h1>
             {selectedPlant ? (
                 <>
@@ -31,7 +36,7 @@ const MyNursery = () => {
                     {myPlants.length > 0 ? (
                         <FetchPlants onSelectPlant={setSelectedPlant} ids={myPlants} />
                     ) : (
-                        <p>No plants in your nursery yet. Visit <u style={{cursor: "pointer"}} onClick={() => navigate("/browse")}>Browse Plants</u></p>
+                        <p>No plants in your nursery yet. Visit <u style={{ cursor: "pointer" }} onClick={() => navigate("/browse")}>Browse Plants</u></p>
                     )}
                 </>
             )}
