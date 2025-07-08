74a10fb3afcdf5dba9f80524305ddb84a9f84904
Federico <alessi.frc@gmail.com>
2025-07-08 17:29:47 +0200

    corrected multiple instances of same plant in nursery bug

--- smart_plant_panel/src/pages/PlantDetails.jsx
+++ smart_plant_panel/src/pages/PlantDetails.jsx
@@ -14,24 +14,26 @@ const PlantDetails = () => {
     const { plants, loading } = useGetPlants({ id: plantId })
     const dispatch = useDispatch()
     const [flag, setFlag] = useState(false)
+    const [message, setMessage] = useState('')
 
-    const nurseryHandler = () => {
-        dispatch(addToNursery(plants))
+    const nurseryHandler = async () => {
+        const result = await dispatch(addToNursery(plants))
+        setMessage(result.message)
         setFlag(true)
         setTimeout(() => { setFlag(false) }, 5000)
     }
 
     return (
         <>
-            {flag ? <Alert severity='success' onClose={() => { setFlag(false) }} id='alert'>Plant added to nursery</Alert> : null}
+            {flag ? <Alert severity='info'  onClose={() => { setFlag(false) }} id='alert'>{message}</Alert> : null}
             <BlankSpace />
             {loading && <SpinnerRoundOutlined />}
             {!loading && (
                 <>
-                <Plant plant={plants} onExit={() => navigate(-1)||navigate("/browse")} />
-                <button onClick={nurseryHandler}>Add to Nursery</button>
+                    <Plant plant={plants} onExit={() => navigate(-1) || navigate("/browse")} />
+                    <button onClick={nurseryHandler}>Add to Nursery</button>
                 </>
-                )}
+            )}
         </>
     )
 }
--- smart_plant_panel/src/redux/NurseryActions.js
+++ smart_plant_panel/src/redux/NurseryActions.js
@@ -1,7 +1,17 @@
-export const addToNursery = (id) => {
-    return { type: "ADD", payload: id }
+export const addToNursery = (plant) => {
+    return async (dispatch, getState) => {
+        const nursery = getState().nursery.plants
+
+        if (!nursery.includes(plant)) {
+            dispatch({type:"ADD", payload: plant})
+            return {ok: true, message: 'Plant added to nursery'}
+        } else {
+            return { ok: false, message: 'Plant already in nursery'}
+        }
+    }
+    //{ type: "ADD", payload: id }
 }
 
-export const removeFromNursery = (id) => {
-    return { type: "REMOVE", payload: id }
+export const removeFromNursery = (plant) => {
+    return { type: "REMOVE", payload: plant }
 }
