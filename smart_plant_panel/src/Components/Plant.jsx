84918d4be66075509c1b9ca263bf5b7f9c177b44
Federico <alessi.frc@gmail.com>
2025-07-09 21:26:28 +0200

    plant updates and css for plant images

--- smart_plant_panel/src/Components/Plant.jsx
+++ smart_plant_panel/src/Components/Plant.jsx
@@ -7,7 +7,7 @@ const Plant = ({ plant, onExit }) => {
         <div>
             <button onClick={() => onExit(null)}>X</button>
             <h1>{plantName}</h1>
-            <img src={`/${image}`} alt={plantName} />
+            <img src={`/${image}`} alt={plantName} id="plant-pic" />
             <br />
             <p><b>Category: </b>{category}</p>
             <p><b>Description: </b>{description}</p>
